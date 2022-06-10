cbc3152d3d31c70c58952ac6c714390dd99e6beb
sachinrawagent <sa147831@gmail.com>
2022-06-10 18:04:47 +0530

    add a motion

--- scratch-block/scratch-blocks-develop/blocks_vertical/motion.js
+++ scratch-block/scratch-blocks-develop/blocks_vertical/motion.js
@@ -582,3 +582,23 @@ Blockly.Blocks['motion_yscroll'] = {
     });
   }
 };
+
+Blockly.Blocks['motion_square'] = {
+  /**
+   * Block to move steps.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.MOTION_SQUARE,
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "STEPS"
+        }
+      ],
+      "category": Blockly.Categories.motion,
+      "extensions": ["colours_motion", "shape_statement"]
+    });
+  }
+};
